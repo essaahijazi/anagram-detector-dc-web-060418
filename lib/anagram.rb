f1597dc379bbe093711530502db337360ca049ce
Essa Hijazi <github email address>
2018-06-10 19:15:58 -0400

    Created a basic anagram class

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    word_to_character = self.word.split("").sort
+    array.select do |possible_match|
+      possible_match.split("").sort == word_to_character
+    end
+  end
+
+end
